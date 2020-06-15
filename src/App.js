ba1bd90b452d1e515ea6ec448aa5b271e748c832
Aleksandr Barmin <albarmin4549@gmail.com>
2020-06-15 08:11:42 +0300

    added component Messages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Messages from './components/Messages/Messages';
 import Profile from './components/Profile/Profile';
 import style from './App.module.scss';
 
@@ -12,6 +13,7 @@ function App() {
             <Header />
             <Navbar />
             <Route exact path={'/'} component={() => <Profile />} />
+            <Route exact path={'/Messages'} component={() => <Messages />} />
         </div>
     );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 
 import style from './Navbar.module.scss';
 
-const Navbar = ({items = ['Home', 'Frends', 'Messages', 'News', 'Settings']}) => {
-  return (
-    <div className={style.navbar__container}>
-        {items.map((item, i) => <Link key={i} to={item === 'Home' ? '/' : item}>{item}</Link>)}
-    </div>
-  );
+const Navbar = ({items = ['Home', 'Messages']}) => {
+    return (
+        <div className={style.navbar__container}>   
+            {items.map((item, i) => <Link key={i} to={item === 'Home' ? '/' : item}>{item}</Link>)}
+        </div>
+    );
 };
 
 Navbar.propTypes = {
