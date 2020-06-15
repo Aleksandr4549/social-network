6a191f61d14d8d9d5e1a4ad62aabe257e8720b6d
Aleksandr Barmin <albarmin4549@gmail.com>
2020-06-15 13:41:35 +0300

    added component Users

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import style from './Navbar.module.scss';
 
-const Navbar = ({items = ['Home', 'Messages']}) => {
+const Navbar = ({items = ['Home', 'Messages', 'Users']}) => {
     return (
         <div className={style.navbar__container}>   
             {items.map((item, i) => <Link key={i} to={item === 'Home' ? '/' : item}>{item}</Link>)}
---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from './Users.module.scss';
+
+const Users = () => {
+    return (
+        <div className={style.users__container}>
+            Users
+        </div>
+    );
+};
+
+export default Users;
